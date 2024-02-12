00e0caf5e88b85c67256ac97cd2c6280f1a10b02
Charles Iheanetu <charlesiheanetu@Charless-MacBook-Air.local>
2024-02-11 19:55:02 -0500

    Initial commit

---
+++ test/dockaws.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Dockaws from '../lib/dockaws-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/dockaws-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Dockaws.DockawsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
